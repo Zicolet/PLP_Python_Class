3d9cbcce4cf356d4e2ad5ce7f5c2b4de1df657f9
Joseph Isaac Ovie <utilism@gmail.com>
2024-02-23 01:16:16 +0100

    week 1

---
+++ user_input.py
@@ -0,0 +1,15 @@
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+location = input('Enter your location: ')
+
+f'Hello {name}, you are {age} years old and you live in {location}'
+
+print('Hello {0}, you are {1} years old and you live in {2}'.format(name , age ,location))
+      
+
+print('Hello {}, you are {} years old and you live in {}'.format(name , age ,location))
+
+
+print('Hello {Name}, you are {Age} years old and you live in {Location}'.format(Name = name , Age = age , Location = location))
+      
+ 
